87f7c4768badd2063f62a53e92922cd208328ea0
tsesme01 <petra.tsesmetzi.ext@ptb.de>
2023-11-09 16:54:28 +0100

    initial

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes:Recipe[]=[
+    new Recipe('A Test Recipe', 'This is a simply test','https://cdn.pixabay.com/photo/2018/06/01/20/28/chicken-3447092_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is a simply test','https://cdn.pixabay.com/photo/2018/06/01/20/28/chicken-3447092_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is a simply test','https://cdn.pixabay.com/photo/2018/06/01/20/28/chicken-3447092_1280.jpg')
+  ];
+
+
+  constructor(){
+
+  }
+
+  ngOnInit() {
+      
+  }
+}
