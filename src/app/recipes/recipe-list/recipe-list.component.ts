42814d2c1f383e25acb17d3eeae27d2c29c3b771
Petra Tsesmetzi <p_tsesmetzi@gmx.de>
2023-11-12 19:25:05 +0100

    Components and Databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import {Recipe } from '../recipe.model';
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes:Recipe[]=[
-    new Recipe('A Test Recipe', 'This is a simply test','https://cdn.pixabay.com/photo/2018/06/01/20/28/chicken-3447092_1280.jpg'),
-    new Recipe('A Test Recipe', 'This is a simply test','https://cdn.pixabay.com/photo/2018/06/01/20/28/chicken-3447092_1280.jpg'),
-    new Recipe('A Test Recipe', 'This is a simply test','https://cdn.pixabay.com/photo/2018/06/01/20/28/chicken-3447092_1280.jpg')
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply test',
+      'https://cdn.pixabay.com/photo/2018/06/01/20/28/chicken-3447092_1280.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe2',
+      'This is a simply test2',
+      'https://cdn.pixabay.com/photo/2018/06/01/20/28/chicken-3447092_1280.jpg'
+    ),
   ];
 
+  constructor() {}
 
-  constructor(){
-
-  }
-
-  ngOnInit() {
-      
+  ngOnInit() {}
+  onRecipeSelected(recipe: Recipe) {
+    console.log('onRecipeSelected');
+    this.recipeWasSelected.emit(recipe);
   }
 }
